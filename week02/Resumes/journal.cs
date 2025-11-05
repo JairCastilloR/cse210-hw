ed3f3422cac81daa95e3ae90b9ce4a4741a7d25f
jaircastillo <fjair.castillo@gmail.com>
2025-11-05 18:26:28 -0500

    week 2 design_journal

---
+++ week02/Resumes/journal.cs
@@ -0,0 +1,32 @@
+using System.Collections.Generic;
+using System.IO;
+public class Journal
+{
+    public List<Entry> _entries;
+
+    public Journal()
+    {
+        
+    }
+
+    public void AddEntry(Entry newEntry)
+    {
+        
+    }
+
+    public void DisplayAll()
+    {
+       
+    }
+
+    public void SaveToFile(string file)
+    {
+        
+    }
+
+    public void LoadFromFile(string file)
+    {
+        
+    }
+
+}
